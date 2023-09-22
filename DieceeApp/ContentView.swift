7e00f21e10251a4f4f7939fb4cecfb302f260822
Ananth Nair <ananthnair96@gmail.com>
2023-09-22 10:53:10 +0530

    Initial Commit

---
+++ DieceeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DieceeApp
+//
+//  Created by Ananth Nair on 22/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DieceeApp/DieceeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DieceeAppApp.swift
+//  DieceeApp
+//
+//  Created by Ananth Nair on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DieceeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
